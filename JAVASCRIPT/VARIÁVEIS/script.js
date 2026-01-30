6946da100d6c661cf9c3d12ada8cdd983221d927
Lucas Rocha <reallucasrocha@gmail.com>
2026-01-30 10:52:55 -0300

    Modificação das pastas de estudos
    Adição das pastas de estudo

---
+++ JAVASCRIPT/VARIÁVEIS/script.js
@@ -0,0 +1,13 @@
+//string
+var texto = "Curso de JavaScript"
+
+//number
+var numeroInteiro = -7
+var numeroFracionado = 11210.75
+
+//bolean
+var teste = true
+
+//alert(numeroFracionado) //abrir um dialog
+//document.write(texto)
+//console.log(numeroInteiro)
